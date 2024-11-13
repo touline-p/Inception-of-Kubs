418e35a2db89e7877b4804530c6c1013862628a5
touline-p <delafforest.brieuc@gmail.com>
2024-11-13 13:28:10 -0500

    first part is good so i commit on main

--- 1/server.sh
+++ 1/server.sh
@@ -1,4 +1,14 @@
-apt-get update
-apt-get install -y 
-apt-get install curl -y
-curl -sfL https://get.k3s.io | sh -
+apt-get update;
+apt-get install curl -y;
+
+#version de k3s
+export INSTALL_K3S_VERSION="v1.28.7+k3s1";
+#droit de lecture de la kube config
+export K3S_KUBECONFIG_MODE="644";
+#flannel is a network fabric to simplify network configurations
+export INSTALL_K3S_EXEC="--flannel-iface=eth1";
+
+curl -sfL https://get.k3s.io | sh -;
+
+# le token est genere dans un file pour qu'il soit reutilise par le worker
+cp /var/lib/rancher/k3s/server/node-token /token/token;
--- 1/worker.sh
+++ 1/worker.sh
@@ -2,5 +2,12 @@ apt-get update
 apt-get install -y 
 apt-get install curl -y
 
+export INSTALL_K3S_VERSION="v1.28.7+k3s1";
+export K3S_KUBECONFIG_MODE="644";
+export INSTALL_K3S_EXEC="--flannel-iface=eth1";
+export K3S_TOKEN_FILE=/token/token;
+export K3S_URL=https://192.168.56.110:6443;
+
 # https://docs.k3s.io/quick-start
-curl -sfL https://get.k3s.io | K3S_URL=https://192.168.56.110:6443 K3S_TOKEN=mynodetoken sh -
+curl -sfL https://get.k3s.io | sh -
+
