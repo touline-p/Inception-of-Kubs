4be14a5e79506eb539d9b1866e1d916ed7a2f37e
bpoumeau <bpoumeau@debian>
2024-11-16 10:53:44 -0500

    part 2

--- 2/server.sh
+++ 2/server.sh
@@ -6,9 +6,13 @@ export INSTALL_K3S_VERSION="v1.28.7+k3s1";
 #droit de lecture de la kube config
 export K3S_KUBECONFIG_MODE="644";
 #flannel is a network fabric to simplify network configurations
-export INSTALL_K3S_EXEC="--flannel-iface=eth1";
+export INSTALL_K3S_EXEC="--flannel-iface=eth1"; 
 
 curl -sfL https://get.k3s.io | sh -;
 
 # le token est genere dans un file pour qu'il soit reutilise par le worker
 cp /shared /protected -r ;
+kubectl apply -f /shared/apps/app1-deployment.yaml
+kubectl apply -f /shared/apps/app2-deployment.yaml
+kubectl apply -f /shared/apps/app3-deployment.yaml
+
