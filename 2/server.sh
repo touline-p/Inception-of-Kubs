361801bee9e1dc1be0c080c60d828fa19e9de89b
Zwhea <theawang@yahoo.com>
2024-11-17 21:23:10 +0100

    modif (part 2) : server.sh was changed to copy and apply the new apps manifests made in requirements. All apps have now their own directory with their kubernetes configurations. A configmap was added for some pzaaaazzzle, it is normally good practice to put env config inside configmaps or secrets (for more sensitive data like passwords). A resources requests was added inside the deployments to prevent the apps from taking too much resources in the machine, also helps with speeeeed. The kustomization file, is what permits the apply -k to work (-k for applying to a directory), you just put everything you wanna apply inside and pffiiiiouuuuu. Labels have also been updated to follow good practices.

--- 2/server.sh
+++ 2/server.sh
@@ -1,18 +1,26 @@
-apt-get update;
-apt-get install curl -y;
+apt-get update && apt-get install curl -y
 
-#version de k3s
-export INSTALL_K3S_VERSION="v1.28.7+k3s1";
-#droit de lecture de la kube config
-export K3S_KUBECONFIG_MODE="644";
-#flannel is a network fabric to simplify network configurations
-export INSTALL_K3S_EXEC="--flannel-iface=eth1"; 
+# version de k3s
+export INSTALL_K3S_VERSION="v1.28.7+k3s1"
+# droit de lecture de la kube config
+export K3S_KUBECONFIG_MODE="644"
+# flannel is a network fabric to simplify network configurations
+export INSTALL_K3S_EXEC="--flannel-iface=eth1" 
 
-curl -sfL https://get.k3s.io | sh -;
+# install K3s
+curl -sfL https://get.k3s.io | sh -
 
 # le token est genere dans un file pour qu'il soit reutilise par le worker
-cp /shared /protected -r ;
-kubectl apply -f /shared/apps/app1-deployment.yaml
-kubectl apply -f /shared/apps/app2-deployment.yaml
-kubectl apply -f /shared/apps/app3-deployment.yaml
+
+# create a backup of our K3s manifests in case the mounted directory becomes unavailable
+cp -r requirements /protected
+
+# wait for K3s to be ready
+until kubectl get nodes | grep -q " Ready"; do
+    echo "Waiting for node to be ready..."
+    sleep 2
+done
+
+# apply / launch all the apps from requirements
+kubectl apply -k requirements/
 
